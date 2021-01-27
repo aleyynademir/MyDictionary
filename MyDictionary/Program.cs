e4b0a09e066a7e5abfbabb91f296495aab58f002
aleyynademir <aleynaberfe4452@gmail.com>
2021-01-27 14:37:33 +0300

    -- Added

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> Student = new Dictionary<int, string>();
+
+            Student.Add(1, "Mehmet");
+            Student.Add(2, "Faruk");
+            Student.Add(3, "Musa");
+            Student.Add(4, "Burak");
+
+            foreach (KeyValuePair<int, string> student in Student)
+            {
+                Console.WriteLine("ID: "+student.Key+"  "+"Adı: "+student.Value);
+            }
+        }
+    }
+}
